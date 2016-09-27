1a32f514a80917f41fd151d0ea752e4b5d02b580
Taff Gao <gaotongfei1995@gmail.com>
2016-09-27 15:26:41 +0800

    fix bug

--- manager.py
+++ manager.py
@@ -1,6 +1,5 @@
 from flask_script import Manager, Server, Shell
 from flask_migrate import Migrate, MigrateCommand
-from flask import current_app
 from corker import create_app, db, models
 
 
@@ -13,7 +12,7 @@ def _make_context():
     return dict(app=app, db=db, models=models)
 
 manager.add_command("db", MigrateCommand)
-manager.add_command("runserver", Server(use_debugger=current_app.config["DEBUG"]))
+manager.add_command("runserver", Server(use_debugger=app.config["DEBUG"]))
 manager.add_command("shell", Shell(make_context=_make_context))
 
 
